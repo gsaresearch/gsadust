4b14e6ce7f9f0a10cfd6ffadfbe3f15e81e11edd
gsaresearch <56109512+gsaresearch@users.noreply.github.com>
2019-11-13 16:29:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+let conStart = 10;
+let conRange = 15;
+let mode = 1;
+
+function setup()
+{
+  let botton1, button2;
+  button1 = createButton('pm2.5');
+  button1.position(10, 100);
+  button1.mouseClicked(button1clicked);
+  button2 = createButton('pm10');
+  button2.position(10, 130);
+  button2.mouseClicked(button2clicked);
+}
+function button1clicked()
+  {
+    mode = 1;
+  };
+function button2clicked()
+  {
+    mode = 2;
+  };
+
 import graph from "./gsadust.js";
 import info from "./gsadust.js";
 import table from "./gsadust.js";
