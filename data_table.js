19c8541b8027e7f5a6c271f59808173be31fd9f0
gsaresearch <56109512+gsaresearch@users.noreply.github.com>
2019-11-13 16:25:34 +0900

    Update data_table.js

--- data_table.js
+++ data_table.js
@@ -1,82 +1,85 @@
-let table = function(p)
+export default function
 {
-  let mode = 1;
-  let f, r;
-  
-  p.preload = function()
+  let table = function(p)
   {
-    table1 = p.loadTable('0828_pm2.5.csv');
-    table2 = p.loadTable('0828_pm10.csv');
-  };
-  
-  p.setup = function() 
-  {
-    p.createCanvas(430, 700);
-    p.frameRate(10);
-  };
-  
-  p.draw = function() 
-  {
-    p.background(0);
-    p.noFill();
-    p.stroke(255);
-    p.textSize(12);
-    
-    for(f = 10; f <= 310; f = f+100)
+    let mode = 1;
+    let f, r;
+
+    p.preload = function()
     {
-      p.rect(f, 10, 100, 20);
-    }
-    p.text('time', 15, 25);
-    p.text('concentration', 115, 25);
-    p.text('latitude', 215, 25);
-    p.text('longitude', 315, 25);
-    
-    if(mode === 1)
+      table1 = p.loadTable('0828_pm2.5.csv');
+      table2 = p.loadTable('0828_pm10.csv');
+    };
+
+    p.setup = function() 
     {
-      for(f = 1; f < table1.getRowCount(); f++)
+      p.createCanvas(430, 700);
+      p.frameRate(10);
+    };
+
+    p.draw = function() 
+    {
+      p.background(0);
+      p.noFill();
+      p.stroke(255);
+      p.textSize(12);
+
+      for(f = 10; f <= 310; f = f+100)
       {
-        row = table1.getRow(f); 
-        for(r = 10; r <= 310; r = r+100)
-        {
-          p.rect(r, 10+ 20*f, 100, 20);
-        }
-        p.text(row.getString(0), 15, 25+ 20*f);
-        p.text(row.getString(1), 115, 25+ 20*f);
-        p.text(row.getString(2), 215, 25+ 20*f);
-        p.text(row.getString(3), 315, 25+ 20*f);
+        p.rect(f, 10, 100, 20);
       }
-    }
-    else
-    {
-      for(f = 1; f < table2.getRowCount(); f++)
+      p.text('time', 15, 25);
+      p.text('concentration', 115, 25);
+      p.text('latitude', 215, 25);
+      p.text('longitude', 315, 25);
+
+      if(mode === 1)
       {
-        row = table2.getRow(f); 
-        for(r = 10; r <= 310; r = r+100)
+        for(f = 1; f < table1.getRowCount(); f++)
         {
-          p.rect(r, 10+ 20*f, 100, 20);
+          row = table1.getRow(f); 
+          for(r = 10; r <= 310; r = r+100)
+          {
+            p.rect(r, 10+ 20*f, 100, 20);
+          }
+          p.text(row.getString(0), 15, 25+ 20*f);
+          p.text(row.getString(1), 115, 25+ 20*f);
+          p.text(row.getString(2), 215, 25+ 20*f);
+          p.text(row.getString(3), 315, 25+ 20*f);
         }
-        p.text(row.getString(0), 15, 25+ 20*f);
-        p.text(row.getString(1), 115, 25+ 20*f);
-        p.text(row.getString(2), 215, 25+ 20*f);
-        p.text(row.getString(3), 315, 25+ 20*f);
       }
-    }
-  };
-  
-  p.mouseWheel = function()
-  {
-    p.translate(0, 10);
-  };
-  
-  p.keyTyped = function()
-  {
-    if(p.key === '1')
+      else
+      {
+        for(f = 1; f < table2.getRowCount(); f++)
+        {
+          row = table2.getRow(f); 
+          for(r = 10; r <= 310; r = r+100)
+          {
+            p.rect(r, 10+ 20*f, 100, 20);
+          }
+          p.text(row.getString(0), 15, 25+ 20*f);
+          p.text(row.getString(1), 115, 25+ 20*f);
+          p.text(row.getString(2), 215, 25+ 20*f);
+          p.text(row.getString(3), 315, 25+ 20*f);
+        }
+      }
+    };
+
+    p.mouseWheel = function()
     {
-      mode = 1;
-    }
-    if(p.key === '2')
+      p.translate(0, 10);
+    };
+
+    p.keyTyped = function()
     {
-      mode = 2;
-    }
+      if(p.key === '1')
+      {
+        mode = 1;
+      }
+      if(p.key === '2')
+      {
+        mode = 2;
+      }
+    };
   };
-};
+}
