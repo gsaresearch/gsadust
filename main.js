4c7d7290d6b31c1f4c45730f0f944c754792a535
gsaresearch <56109512+gsaresearch@users.noreply.github.com>
2019-11-13 16:16:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import graph from "gsadust.js";
-import info from "gsadust.js";
-import table from "gsadust.js";
+import graph from "./gsadust.js";
+import info from "./gsadust.js";
+import table from "./gsadust.js";
 
 new p5(graph);
 new p5(info);
