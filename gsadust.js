d38a3482d408d92b1d4bd75935bb2c21a177b5d1
gsaresearch <56109512+gsaresearch@users.noreply.github.com>
2019-11-27 17:27:20 +0900

    Update gsadust.js

--- gsadust.js
+++ gsadust.js
@@ -150,6 +150,12 @@ let graph = function(p)
   p.setup = function()
   {
     p.createCanvas(700, 700, p.P2D);
+    p.text('위도(°)', 10, 350);
+    p.text('35.22902778°',10,10);
+    p.text('35.22697222',10,600);
+    p.text('경도(°)', 350, 600);
+    p.text('126.8471111°',10,600);
+    p.text('126.8496389°',600,600);
   };
 
   p.draw = function()
